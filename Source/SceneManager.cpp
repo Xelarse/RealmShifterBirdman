2b85e3aea80d2d426ee48974b0e6ee700418c5d5
AlexAllmanLaptop <xalexxd@hotmail.co.uk>
2018-02-10 14:39:56 +0000

    Boilerplate code setup for SceneManager

---
+++ Source/SceneManager.cpp
@@ -0,0 +1,13 @@
+#include "SceneManager.h"
+
+void SceneManager::clearScene(SCENE scene)
+{
+}
+
+void SceneManager::renderScene(SCENE scene, ASGE::Renderer * renderer)
+{
+}
+
+void SceneManager::addNodeToScene(SCENE scene, NODE node)
+{
+}
